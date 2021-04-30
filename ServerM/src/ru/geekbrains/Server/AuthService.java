7514d82ed5ca7a924d73bc1231def4bf988d2f05
Vera <mverynchik@gmail.com>
2021-04-30 03:40:10 +0300

    timeout

--- ServerM/src/ru/geekbrains/Server/AuthService.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.Server;
-
-public interface AuthService {
-    void start();
-    String getNickByLoginPass(String login, String password);
-    void stop();
-}
--- ServerM/src/ru/geekbrains/Server/ServerApp.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.Server;
-
-public class ServerApp {
-    public static void main(String[] args) {
-        new MyServer();
-    }
-}
